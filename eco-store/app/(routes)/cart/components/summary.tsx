c7a578bad607dd4eb45b7bad1a5f9549a7f77bb9
nhquy93 <nhquy93@gmail.com>
2023-07-14 14:58:17 +0700

    Add test payment feature

--- eco-store/app/(routes)/cart/components/summary.tsx
+++ eco-store/app/(routes)/cart/components/summary.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import axios from "axios";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
 import { useSearchParams } from "next/navigation";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 
 import useCart from "@/hooks/use-cart";
-import { useEffect } from "react";
-import { toast } from "react-hot-toast";
+
 
 const Summary = () => {
     const searchParams = useSearchParams();
